7ca67d74a1809aae307c32fd0bd78285c961d718
Gino Tasis <gtasis84@gmail.com>
2022-03-29 19:24:14 -0400

    Initial Commit

---
+++ Flavorfool/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flavorfool
+//
+//  Created by Gino Tasis on 3/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
